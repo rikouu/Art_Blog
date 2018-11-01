79f701bce0ce1a7139714ca0af85e1cac6543e29
lijun <lijun@021.com>
2018-11-01 18:30:03 +0800

    新增gulp配置

---
+++ src/js/javascript.js
@@ -0,0 +1,383 @@
+$(function () {
+    //网站预加载运动开始
+    if ($(document).width() >= 1200) {
+        if (window.location.href == "https://www.weipxiu.com" || window.location.href == "https://www.weipxiu.com/") {
+            $("#js_banner").show();
+            //首页公告开始
+            setInterval(function () {
+                $(".notice").show();
+                $(".notice ul").stop(true, true).animate({
+                    "top": "-20px"
+                }, function () {
+                    var node = $(".notice ul li:eq(0)").remove();
+                    $(".notice ul").append(node);
+                    $(".notice ul").css("top", "0");
+                });
+            }, 5000);
+
+            //轩枫博客start
+            //turnEffect（翻转）boomEffect（爆炸）pageEffect（翻页）skewEffect（扭曲）cubeEffect（立方体）
+            var flippingMode = ['turnEffect', 'boomEffect', 'skewEffect']
+            var randomNum = Math.floor(Math.random() * 3);
+            var banner = new Banner({
+                banner: '#banner_img',
+                index: 0,
+                autoplay: 8000,
+                width: 1200,
+                height: 300,
+                images: [{
+                    url: 'https://www.weipxiu.com/wp-content/themes/boke/images/banner-1.jpg',
+                    link: 'https://www.weipxiu.com/?cat=29'
+                }, {
+                    url: 'https://www.weipxiu.com/wp-content/themes/boke/images/banner-2.jpg',
+                    link: 'https://www.weipxiu.com/'
+                }, {
+                    url: 'https://www.weipxiu.com/wp-content/themes/boke/images/banner-3.jpg',
+                    link: 'javascript:;'
+                }, {
+                    url: 'https://www.weipxiu.com/wp-content/themes/boke/images/banner-4.jpg',
+                    link: 'javascript:;'
+                }, {
+                    url: 'https://www.weipxiu.com/wp-content/themes/boke/images/banner-5.jpg',
+                    link: 'https://www.weipxiu.com/?cat=6'
+                },
+                {
+                    url: 'https://www.weipxiu.com/wp-content/themes/boke/images/banner-6.jpg',
+                    link: 'https://www.weipxiu.com/?p=1313'
+                }
+                ],
+
+                preloadImages: true,// 预加载所有图片
+
+                // 分页及控制
+                pagination: '.js_banner_nav',     // 分页dom
+                paginationClick: true,  // 分页是否可点击
+                prevButton: '.js_banner_prev',     // 下一张dom
+                nextButton: '.js_banner_next',     // 上一张dom
+                Effects: {
+                    //turnEffect（翻转）boomEffect（爆炸）pageEffect（翻页）skewEffect（扭曲）cubeEffect（立方体）
+                    'prev': 'turnEffect',
+                    'next': 'boomEffect',
+                    'navi': 'pageEffect'
+                },
+            });
+            //轩枫博客end
+
+            //给首页增加默认高亮
+            $(".nav ul.music-nav li:eq(0)").addClass("action");
+
+            /*//视频播放start
+            var delSetInterval = null; //定时器
+            var ISvideo = false; //当前是否全屏
+            var myPlayer = videojs( 'my-video' );
+            var howMuchIsDownloaded = 0; //初始化缓冲百分比
+            var eleFull = document.querySelector( "#my-video" ); //视频对象
+
+            //视频全屏方法
+            (function () {
+                var runPrefixMethod = function (element, method) {
+                    var usablePrefixMethod;
+                    ["webkit", "moz", "ms", "o", ""].forEach(function (prefix) {
+                        if (usablePrefixMethod) return;
+                        if (prefix === "") {
+                            // 无前缀，方法首字母小写
+                            method = method.slice(0, 1).toLowerCase() + method.slice(1);
+
+                        }
+                        var typePrefixMethod = typeof element[prefix + method];
+                        if (typePrefixMethod + "" !== "undefined") {
+                            if (typePrefixMethod === "function") {
+                                usablePrefixMethod = element[prefix + method]();
+                            } else {
+                                usablePrefixMethod = element[prefix + method];
+                            }
+                        }
+                    });
+                    return usablePrefixMethod;
+                };
+                if (typeof window.screenX === "number") {
+                    eleFull.addEventListener("dblclick", function () {
+                        if (runPrefixMethod(document, "FullScreen") || runPrefixMethod(document, "IsFullScreen")) {
+                            runPrefixMethod(document, "CancelFullScreen");
+                            this.title = this.title.replace("退出", "");
+                        } else if (runPrefixMethod(this, "RequestFullScreen")) {
+                            this.title = this.title.replace("点击", "点击退出");
+                        }
+                    });
+                } else {
+                    alert("爷，现在是年轻人的时代，您就暂且休息去吧~~");
+                }
+            })();
+            
+            //初始化加载需要先缓冲到15%+才会播放，避免高清视频卡顿
+            delSetInterval = setInterval( function() {
+                howMuchIsDownloaded = myPlayer.bufferedPercent() //返回当前百分比缓冲0-1
+                //console.log(howMuchIsDownloaded*100 + '%')
+                if ( howMuchIsDownloaded > 0.15 ) {
+                    clearInterval( delSetInterval )
+                    myPlayer.play();
+                }
+            }, 1500 )
+
+            //当视频播放完成后，重新加载渲染，随时准备第二次重播
+            myPlayer.on("ended", function () {
+                //alert("视频已播放完成");
+                myPlayer.play();
+                setTimeout(function () {
+                    myPlayer.pause();
+                }, 2000);
+            });
+            //视频播放end*/				
+
+            // 桌面提醒功能
+            var set_desktop = function () {
+                if (window.Notification) {
+                    // var button = document.getElementById('button'), text = document.getElementById('text');
+
+                    var popNotice = function () {
+                        if (Notification.permission == "granted") {
+                            var notification = new Notification("官方提示：", {
+                                body: '欢迎点击加入"WEB前端薪资吐槽群"互相学习、交流！',
+                                icon: 'https://www.weipxiu.com/wp-content/themes/boke/images/tishi.jpg'
+                            })
+
+                            notification.onclick = function () {
+                                window.open("https://jq.qq.com/?_wv=1027&k=4BemYKg")
+                                notification.close();
+                            }
+                        }
+                    }
+
+                    var desktop = function () {
+                        if (Notification.permission == "granted") {
+                            popNotice();
+                        } else if (Notification.permission != "denied") {
+                            Notification.requestPermission(function (permission) {
+                                popNotice();
+                            })
+                        }
+                    }
+                    desktop();
+                } // else {
+                //     alert('浏览器不支持Notification');    
+                // }
+            }
+            //set_desktop();
+            setTimeout(function () {
+                set_desktop();
+            }, 20000);
+            // 桌面提醒功能
+
+            // setTimeout(function(){
+            // 	var swiper = new Swiper('.swiper-container', {
+            // 	    pagination: '.swiper-pagination',//是否出现小圆点
+            // 	    nextButton: '.swiper-button-next',//上一张
+            // 	    prevButton: '.swiper-button-prev',//下一张
+            // 	    slidesPerView: 1,//每一屏幕排几张图片
+            // 	    effect: 'slide',//轮播方式，左右切换
+            // 	    paginationClickable: true,//小圆点是否可点击
+            // 	    spaceBetween: 0,//图片间距
+            // 	    autoplay: 4500,//自动轮播时间
+            // 	    speed:350,//切换一张所需要的时间
+            // 	    keyboardControl : true,//键盘左右按钮切换
+            // 	    mousewheelControl : false,//鼠标滚轮切换
+            // 	    autoplayDisableOnInteraction : false,//表示用户操作swiper之后，是否禁止autoplay。默认为 true：停止。false是播放
+            // 	    loop: true//循环
+            // 	});
+            // },3000);
+
+            // console.log---start
+            if (window.console && window.console.log) {
+                setTimeout(function () {
+                    console.log("\n %c 唯品秀个人博客 %c  © Jun Li  https://weipxiu.com \n",
+                        "color:#FFFFFB;background:#1abc9c;padding:5px 0;border-radius:.5rem 0 0 .5rem;",
+                        "color:#FFFFFB;background:#080808;padding:5px 0;border-radius:0 .5rem .5rem 0;"
+                    );
+                }, 1500);
+            }
+            // console.log---end
+
+            // 首页弹窗开始
+            setTimeout(function () {
+                var oBox = document.getElementById("curriculum");
+                var oUl = oBox.getElementsByTagName("ul")[0];
+                var oContact = oBox.getElementsByClassName("contact")[0];
+                var oClos = oBox.getElementsByClassName("close")[0];
+                var aLi = oUl.children;
+                var oBack = oContact.getElementsByTagName("a")[0];
+                oBox.style.opacity = 1;
+                oBox.addEventListener("transitionEnd", end, false);
+                oBox.addEventListener("webkitTransitionEnd", end, false);
+                //oUl.style.transition=".5s 600ms linear";
+                function end() {
+                    this.removeEventListener("transitionEnd", end, false);
+                    this.removeEventListener("webkitTransitionEnd", end, false);
+                    oUl.style.top = 0;
+                    oBox.style.height = "230px";
+                    oBox.style.top = "0px";
+                    for (var i = 0; i < aLi.length; i++) {
+                        aLi[i].style.transition = "0.5s " + (300 + i * 200) + "ms";
+                        aLi[i].style.opacity = 1;
+                        aLi[i].style.transform = "rotateX(0deg)";
+                        aLi[i].style.WebkitTransform = "rotateX(0deg)";
+                        aLi[i].off = true;
+                        aLi[i].index = i;
+                        aLi[i].onmouseover = over;
+                        aLi[i].onmouseout = function () {
+                            if (this.off) {
+                                this.style.transform = "rotateY(0deg)";
+                                this.style.WebkitTransform = "rotateY(0deg)";
+                            }
+                        };
+                        aLi[i].onclick = fnClick;
+                    }
+                }
+
+                function over(ev) {
+                    if (this.off) {
+                        var iX = ev.clientX - getLeft(this);
+                        this.style.transition = "0.5s";
+                        if (iX > this.offsetWidth / 2) {
+                            this.style.transform = "rotateY(30deg)";
+                            this.style.WebkitTransform = "rotateY(30deg)";
+                        } else {
+                            this.style.transform = "rotateY(-30deg)";
+                            this.style.WebkitTransform = "rotateY(-30deg)";
+                        }
+                    }
+                }
+
+                function getLeft(obj) {
+                    var iLeft = 0;
+                    while (obj) {
+                        iLeft += obj.offsetLeft;
+                        obj = obj.offsetParent;
+                    }
+                    return iLeft
+                }
+
+                function fnClick(ev) {
+                    var iX = ev.clientX - getLeft(this);
+                    var iDeg = iX > this.offsetWidth / 2 ? -180 : 180;
+                    var iMax = 0;
+                    var iNow = 0;
+                    oContact.style.display = "block";
+                    for (var i = 0; i < aLi.length; i++) {
+                        if (iMax < Math.abs(i - this.index)) {
+                            iMax = Math.abs(i - this.index);
+                            iNow = i;
+                        }
+                        aLi[i].off = false;
+                        aLi[i].style.transition = "0.5s " + Math.abs(i - this.index) * 100 + "ms cubic-bezier(0.115, -0.195, 0.255, -0.280)";
+                        aLi[i].style.transform = "rotateY(" + iDeg + "deg)";
+                        aLi[i].style.WebkitTransform = "rotateY(" + iDeg + "deg)";
+                        aLi[i].style.opacity = 0.1;
+                    }
+                    aLi[iNow].addEventListener("transitionEnd", end, false);
+                    aLi[iNow].addEventListener("webkitTransitionEnd", end, false);
+
+                    function end() {
+                        this.removeEventListener("transitionEnd", end, false);
+                        this.removeEventListener("webkitTransitionEnd", end, false);
+                        oContact.style.opacity = 1;
+                    }
+                }
+                oBack.onclick = function () {
+                    oContact.style.opacity = 0;
+                    oContact.addEventListener("transitionEnd", end, false);
+                    oContact.addEventListener("webkitTransitionEnd", end, false);
+
+                    function end() {
+                        this.removeEventListener("transitionEnd", end, false);
+                        this.removeEventListener("webkitTransitionEnd", end, false);
+                        for (var i = 0; i < aLi.length; i++) {
+                            aLi[i].off = true;
+                            aLi[i].style.transition = "0.5s " + (aLi.length - i - 1) * 100 + "ms";
+                            aLi[i].style.transform = "rotateY(0deg)";
+                            aLi[i].style.WebkitTransform = "rotateY(0deg)";
+                            aLi[i].style.opacity = 1;
+                        }
+                    }
+                };
+
+                function Clos() {
+                    oBox.style.transition = ".8s height,0.4s opacity .2s";
+                    oBox.style.height = "40px";
+                    oBox.style.opacity = 0;
+                    setTimeout(function () {
+                        $("#curriculum").remove();
+                    }, 1000)
+                };
+                oClos.onclick = function () {
+                    Clos();
+                }
+                setTimeout(function () {
+                    Clos()
+                }, 8000);
+                document.onkeydown = function (e) {
+                    if (!e) e = window.event;
+                    if ((e.keyCode || e.which) == 13) {
+                        Clos();
+                    };
+                }
+            }, 3000);
+            // 首页弹窗结束
+
+            $("#hide").show();
+            $("html").css({
+                "overflow-y": "visible",
+                "height": "auto",
+                "width": "auto"
+            });
+            $("body").css({
+                "overflow-x": "hidden",
+                "height": "auto",
+                "width": "auto"
+            });
+            $(".buffer").fadeOut();
+            $(".buffer .bar").hide();
+
+            //首页头部导航动画加载
+            $(".header.Top").css("WebkitAnimation-duration", ".7s");
+            $(".header.Top").css("MsAnimation-duration", ".7s");
+            $(".header.Top").css("animation -duration", ".7s");
+
+            //开场背景音乐
+            //$("#music").get(0).play();
+            setTimeout(function () {
+                $("#vedio").animate({
+                    "bottom": "0"
+                }, 1000);
+                $(".hide").animate({
+                    "bottom": "193px"
+                }, 1000);
+            }, 3500);
+        } else {
+            $(".continar").css("margin-top", "128px");
+            $(".c-860,.index-box,.hide,#curriculum,#js_banner").remove(); //去掉轮播、视频、邮件订阅
+        }
+    } else {
+        //排除PC端执行下列代码
+        //去掉首页列表第一篇文章，避免同今日焦点栏目顶替文章重合
+        $(".continar-left > .text:nth-of-type(3)").remove();
+        //swiper核心三要素：依赖swiper.js、swiper.css，外面父亲盒子高度
+        var swiper1 = new Swiper('.swiper-container1', {
+            pagination: '.swiper-pagination', //是否出现小圆点
+            //nextButton: '.swiper-button-next',//上一张
+            //prevButton: '.swiper-button-prev',//下一张
+            slidesPerView: 1, //每一屏幕排几张图片
+            effect: 'slide', //轮播方式，左右切换
+            paginationClickable: true, //小圆点是否可点击
+            spaceBetween: 0, //图片间距
+            autoplay: 4500, //自动轮播时间
+            speed: 500, //切换一张所需要的时间
+            keyboardControl: true, //键盘左右按钮切换
+            mousewheelControl: false, //鼠标滚轮切换
+            autoplayDisableOnInteraction: false, //表示用户操作swiper之后，是否禁止autoplay。默认为 true：停止。false是播放
+            loop: true //循环
+        });
+        //navigator.vibrate([1000, 500, 1000]);
+        //手机震动功能，里面是数组-震动时间，第二个为间隔时间
+    }
+    //网站预加载运动结束
+})
---
+++ src/page.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!--声明当前页面的编码集charset=gbk中文编码gb2312,charset=utf-8 国际编码-->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <!--当前页面的三要素-->
+    <title>登录注册</title>
+    <meta name="Keywords" content="关键词,关键词">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=no">
+    <link rel="shortcut icon" type="image/x-icon" href="<?php bloginfo('template_url'); ?>/favicon.ico" />
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" media="screen and (max-width:767px)" href="<?php bloginfo('template_url'); ?>/css/style-ios.css">
+    <link rel="stylesheet" type="text/css" media="screen and (min-width:768px) and (max-width:1199px)" href="<?php bloginfo('template_url'); ?>/css/style-ipd.css">
+    <link rel="stylesheet" type="text/css" media="screen and (min-width:1200px)" href="<?php bloginfo('template_url'); ?>/style.css">
+    <?php wp_head(); ?>
+    <style>
+        /*会员登录注册开始*/
+
+        #WebCon {
+            width: 330px;
+            margin: 50px auto 0;
+            background: #fff;
+            padding: 35px 20px 30px 31px;
+            box-shadow: 2px 2px 8px rgba(0, 0, 0, .2);
+            border-radius: 7px;
+        }
+
+        #web_page {
+            color: #999
+        }
+
+        #web_page .wpuf-message {
+            text-align: center;
+            font-size: 18px;
+            color: #ed145b;
+        }
+
+        #web_page label {
+            display: inline-block;
+            width: 74px;
+        }
+
+        #web_page p {
+            line-height: 45px;
+            margin-top: 10px;
+            height: 45px;
+        }
+
+        #web_page input {
+            height: 38px;
+            border: 1px solid #bbb;
+            text-indent: 2px;
+            border-radius: 5px;
+            width: 185px
+        }
+
+        #web_page #wp-submit {
+            background: #04A4CC;
+            color: #fff;
+            text-align: center;
+            font-size: 14px;
+            display: block;
+            margin: 0 auto;
+            padding: 8px 30px;
+            width: 150px;
+            cursor: pointer;
+        }
+
+        #web_page #wpuf-rememberme {
+            width: 12px;
+            height: 12px;
+            margin-top: 18px;
+            cursor: pointer;
+        }
+
+        #web_page a {
+            display: inline-block;
+            text-align: center;
+            margin: 20px 42px 0;
+        }
+
+        .forgetmenot {
+            height: 26px;
+            line-height: 26px
+        }
+
+        #WebCon .forgetmenot label {
+            width: 170px;
+        }
+
+        #WebCon .forgetmenot label[for*="wpuf-rememberme"] {
+            width: 115px
+        }
+
+        #web_page p[class*="forgetmenot"] {
+            text-align: center;
+        }
+
+        #web_page p input[name*="user_login"] {
+            width: 267px;
+            display: block;
+        }
+
+        #web_page #lostpasswordform label[for*="wpuf-user_login"] {
+            width: 100%;
+        }
+
+        #web_page #wp-submit[value*="获取新密码"] {
+            margin-top: 60px;
+        }
+
+        span.wpuf-user-avatar img {
+            display: block;
+            margin: 0 auto;
+            border-radius: 100%
+        }
+
+        .wpuf-user-loggedin>ul li {
+            margin-top: 30px;
+        }
+
+        #WebCon .wpuf-user-loggedin>a {
+            margin: 20px 0px 3px 43px;
+        }
+
+        .ds-login {
+            display: none
+        }
+        /*会员登录注册结束*/
+    </style>
+    <?php wp_head(); ?>
+</head>
+
+<body class="webBody">
+    <?php get_header(); ?>
+    <?php  if (is_user_logged_in()){
+  		echo '<script>window.location.href="https://www.weipxiu.com"</script>';
+	}/*用户登录成功立即跳转到网站首页*/
+?>
+    <!-- 正文区域start -->
+    <div class="continar" style="width:1000px; min-height:445px">
+        <div id="WebCon">
+            <div id="web_page">
+                <?php if(have_posts()) : ?>
+                <?php while(have_posts()) : the_post(); ?>
+                <?php the_content(); ?>
+                <?php endwhile; ?>
+                <?php endif; ?>
+                <div style="clear:both"></div>
+            </div>
+        </div>
+    </div>
+    <!-- 正文区域结束 -->
+    <?php get_footer() ?>
+    <?php wp_footer(); ?>
+</body>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/index.js"></script>
+<script>
+    $( function() {
+        $( "#wpuf-avatar-pickfiles" ).parents( ".continar" ).css( {
+            "min-height": "600px",
+            "margin-bottom": "60px",
+            "width": "560px"
+        } );
+        $( "#wpuf-avatar-pickfiles" ).parents( "#WebCon" ).css( "width", "100%" );
+        $( "#wpuf-login-form a:last" ).html( "忘记密码？" );
+        $( "#lostpasswordform" ).prev().html( "请输入您注册时填写的电子邮件账号。您将收到一个链接，以通过电子邮件创建一个新的密码！" );
+        //移动端搜索区域结束
+        if ( $( "body,html" ).width() < 960 ) {
+            $( ".continar" ).css( "width", "95%" );
+        }
+    } )
+</script>
+</html>
