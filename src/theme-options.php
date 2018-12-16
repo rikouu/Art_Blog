9ed73c783f9dbf11026db9aa294c15a77e1646b7
李俊MacBook <lijun@021.com>
2018-12-16 11:16:07 +0800

    更新主题设置

--- src/theme-options.php
+++ src/theme-options.php
@@ -1,8 +1,6 @@
 <?php
- 
     function getOptions() {
         $options = get_option('cnblogs_options');
-        
         if (!is_array($options)) {
             $options['meta_keywords'] = '';
             update_option('cnblogs_options', $options);
@@ -19,7 +17,6 @@
         } else {
             getOptions();
         }
- 
         add_theme_page("唯品秀主题设置", "唯品秀主题设置", 'edit_themes', basename(__FILE__),  'display');
     }
  
@@ -27,31 +24,39 @@
     function display() {
         $options = getOptions();
 ?>
- 
+ <style>
+ #op_form table{
+     width:100%;
+ }
+ </style>
 <form action="#" method="post" enctype="multipart/form-data" name="op_form" id="op_form">
     <div class="wrap">
         <h2>当前主题选项</h2>
-        
         <table>
             <tbody>
                 <tr>
-                    <td>文本</td>
+                    <td>博客简洁</td>
                     <td>
                         <label>
-                            <textarea name="meta_keywords" cols="200" rows="5" id="meta_keywords" style="width:98%;" ><?php echo($options['meta_keywords']); ?></textarea>
+                            <textarea name="meta_keywords" cols="200" id="meta_keywords" style="width:98%;" ><?php echo($options['meta_keywords']); ?></textarea>
+                        </label>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <label>
+                            <textarea name="meta_keywords" cols="200" id="meta_keywords" style="width:98%;" ><?php echo($options['meta_keywords']); ?></textarea>
                         </label>
                     </td>
                 </tr>
             </tbody>
         </table>
- 
         <p class="submit">
             <input type="submit" name="input_save" value="保存" />
         </p>
     </div>
- 
 </form>
- 
 <?php
     }
  
